5d9c41f7d81e30303bb1eb7b6b6aa6b9ffb55a25
Pedro Pinto <xten@osec.io>
2023-10-12 19:11:14 -0300

    Last update of v1

--- harness.py
+++ harness.py
@@ -2,6 +2,7 @@
 import afl
 import os
 import subprocess
+import time
 activate_venv_file = "./venv/bin/activate_this.py"
 
 # TODO: Create some class that describes the program state for a vyper contract
@@ -31,6 +32,7 @@ def main():
     # - untrack coverage from the interpreter
     # - facilitate isolating the 2 titanboa versions
     try:
+        start = time.time()
         interpreted = subprocess.run(
             ["python3", "interpret.py", program],
             stdout=subprocess.PIPE,
@@ -38,14 +40,22 @@ def main():
             ).stdout.decode().split(":")[1].strip()
         if interpreted == "InterpretationFailed":
             os._exit(0)
+        end = time.time()
+        print(f"time to interpret: {end - start}")
         print(f"interpreted: {interpreted}")
 
         # Switch to venv
         exec(open(activate_venv_file).read(), {'__file__': activate_venv_file})
+        start = time.time()
         import boa
+        end = time.time()
+        print(f"time to load boa: {end - start}")
 
         # Run against compiler
+        start = time.time()
         compiled = str(boa.loads(program).main())
+        end = time.time()
+        print(f"time to compile and run: {end - start}")
     except:
         os._exit(0)
     print(f"compiled: {compiled}")
